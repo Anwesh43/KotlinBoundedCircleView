f23f64756128ffff015d771e77773deec2803398
Anwesh43 <anweshthecool0@gmail.com>
2018-05-12 14:11:10 +0530

    adding views in activity

--- app/src/main/java/com/example/anweshmishra/kotlinboundedcircleview/MainActivity.kt
+++ app/src/main/java/com/example/anweshmishra/kotlinboundedcircleview/MainActivity.kt
@@ -2,11 +2,38 @@ package com.example.anweshmishra.kotlinboundedcircleview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.boundedcircleview.BoundedCircleView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        BoundedCircleView.create(this, 0f, 0f, 250, 250)
+
+        BoundedCircleView.create(this, 300f, 0f, 250, 250)
+
+        BoundedCircleView.create(this, 600f, 0f, 250, 250)
+
+        BoundedCircleView.create(this, 0f, 300f, 250, 250)
+
+        BoundedCircleView.create(this, 300f, 300f, 250, 250)
+
+        BoundedCircleView.create(this, 600f, 300f, 250, 250)
+
+
+        BoundedCircleView.create(this, 0f, 600f, 250, 250)
+
+        BoundedCircleView.create(this, 300f, 600f, 250, 250)
+
+        BoundedCircleView.create(this, 600f, 600f, 250, 250)
+
+        BoundedCircleView.create(this, 0f, 900f, 250, 250)
+
+        BoundedCircleView.create(this, 300f, 900f, 250, 250)
+
+        BoundedCircleView.create(this, 600f, 900f, 250, 250)
+
+        BoundedCircleView.create(this, 0f, 1200f, 250, 250)
+
     }
 }
